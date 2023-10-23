5914de895ca06012d9f87b9cb20aeb13d13eba07
PJWoods17 <37152816+PJWoods17@users.noreply.github.com>
2023-10-23 04:14:13 -0500

    map works!!!

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 var gMap;
-function initGMap( ){
+function initMap( ){
     gMap = new google.maps.Map(document.getElementById('myMapID'), {
         center: {lat: 41.878, lng:10}, zoom:3});
 
 }
-function initapplication(){
+function initApplication(){
     console.log("Map Mania lite is starting up!")
 }
